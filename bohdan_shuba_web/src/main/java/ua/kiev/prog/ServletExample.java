5bb2fdf5478321b169ed67a6b647f35b68627a98
BohdanShuba <shubabohdan@gmail.com>
2019-01-21 21:16:45 +0200

    add module "bohdan_shuba_web"

---
+++ bohdan_shuba_web/src/main/java/ua/kiev/prog/ServletExample.java
@@ -0,0 +1,17 @@
+package ua.kiev.prog;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/serv")
+public class ServletExample extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+      resp.getWriter().println("<h2>Hello from servlet</h2>");
+    }
+}
