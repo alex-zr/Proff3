63464310471fb014f1f13f4427a548663a98a8f7
EduardTymchenko <timchenko_eduard@ukr.net>
2019-01-22 23:08:36 +0200

    My first servlet

---
+++ eduard_tymchenko_web/src/main/java/ua/kiev/prog/ServletExample.java
@@ -0,0 +1,16 @@
+package ua.kiev.prog;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/serv")
+public class ServletExample extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().println("<h2>Hello from servlet :-)</h2>");
+    }
+}
