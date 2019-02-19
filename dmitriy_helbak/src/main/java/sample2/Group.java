d9f60afc7a2a17b04c3bd8171735186445451cc3
Helbak <helbak77@gmail.com>
2019-02-19 19:40:11 +0200

    dmitriy_helbak added a class MyMainClass

---
+++ dmitriy_helbak/src/main/java/sample2/Group.java
@@ -0,0 +1,61 @@
+package sample2;
+
+import javax.persistence.*;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "Groups")
+public class Group {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "group",
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private Set<Client> clients = new HashSet<>();
+
+    public Group() {
+    }
+
+    public Group(String name) {
+        this.name = name;
+    }
+
+    public void addClient(Client client) {
+        client.setGroup(this);
+        clients.add(client);
+    }
+
+    public Set<Client> getClients() {
+        return Collections.unmodifiableSet(clients);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
