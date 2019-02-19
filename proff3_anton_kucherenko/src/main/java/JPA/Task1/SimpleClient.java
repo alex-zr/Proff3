1a3bab8dc33c2395fffcc44dab8f7edfb1b42020
AntonKucherenko <44853465+AntonKucherenko@users.noreply.github.com>
2019-02-19 23:40:12 +0200

    first spring commit

---
+++ proff3_anton_kucherenko/src/main/java/JPA/Task1/SimpleClient.java
@@ -0,0 +1,57 @@
+package JPA.Task1;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "clients")
+public class SimpleClient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+    private Integer age;
+
+    public SimpleClient() {
+    }
+
+    public SimpleClient(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "SimpleClient{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
