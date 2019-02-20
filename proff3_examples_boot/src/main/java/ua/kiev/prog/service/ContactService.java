10f0761f12685d8d1bf4b8cdf610aa8cf2cca678
alex-zr <oleksandr.roshchupkin@gmail.com>
2019-02-20 20:42:49 +0200

    some refactoring

--- proff3_examples_boot/src/main/java/ua/kiev/prog/service/ContactService.java
+++ proff3_examples_boot/src/main/java/ua/kiev/prog/service/ContactService.java
@@ -1,7 +1,6 @@
 package ua.kiev.prog.service;
 
 import org.springframework.data.domain.Pageable;
-import org.springframework.transaction.annotation.Transactional;
 import ua.kiev.prog.domain.Contact;
 import ua.kiev.prog.domain.Group;
 
@@ -10,10 +9,8 @@ import java.util.Optional;
 
 public interface ContactService {
 
-    @Transactional
     void addContact(Contact contact);
 
-    @Transactional
     void addGroup(Group group);
 
     void deleteContacts(long[] idList);
@@ -24,12 +21,10 @@ public interface ContactService {
 
     List<Contact> findByGroup(Group group, Pageable pageable);
 
-    @Transactional(readOnly = true)
     long countByGroup(Group group);
 
     List<Contact> findByPattern(String pattern, Pageable pageable);
 
-    @Transactional(readOnly = true)
     long count();
 
     Optional<Group> findGroup(long id);
