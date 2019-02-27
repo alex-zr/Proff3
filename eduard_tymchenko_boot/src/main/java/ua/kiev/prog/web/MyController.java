279e57050ef339902053cae1f55222dc3911fc00
Helbak <helbak77@gmail.com>
2019-02-27 20:56:35 +0200

    dmitriy_helbak added a method autoUserPage

--- eduard_tymchenko_boot/src/main/java/ua/kiev/prog/web/MyController.java
+++ eduard_tymchenko_boot/src/main/java/ua/kiev/prog/web/MyController.java
@@ -35,6 +35,17 @@ public class MyController {
 
         return "index";
     }
+//team trial
+
+    @RequestMapping("/auto_user")
+    public String autoUserPage(Model model) {
+//        model.addAttribute("groups", contactService.findGroups());
+        return "auto_user_page";
+    }
+
+
+
+
 
     @RequestMapping("/contact_add_page")
     public String contactAddPage(Model model) {
